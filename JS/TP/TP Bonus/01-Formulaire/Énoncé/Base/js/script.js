bd8b94714b45041d35cde01e08005f1d771a7453
Fournier Corinna <122286937+Corinna28@users.noreply.github.com>
2023-11-09 16:46:55 +0100

    maj

--- JS/TP/TP Bonus/01-Formulaire/Énoncé/Base/js/script.js
+++ JS/TP/TP Bonus/01-Formulaire/Énoncé/Base/js/script.js
@@ -2,17 +2,55 @@
 
 // fonction pour mettre le champ demandé (field) en rouge (classe CSS "field-invalid") et insérer un message d'erreur (errorText) en dessous du champ qui aura la class css "error-text".
 
+// Fonction pour ajouter la classe field-invalid, créer et afficher un message d'erreur
+// Fonction pour ajouter la classe field-invalid, créer et afficher un message d'erreur
+
+function afficherErreur(champ, message) {
+    // Ajouter la classe field-invalid sur le champ demandé
+    champ.classList.add("field-invalid");
+  
+    // Créer une div pour le message d'erreur
+    const erreurDiv = document.createElement("div");
+  
+    // Ajouter la classe error-text à la div
+    erreurDiv.classList.add("error-text");
+  
+    // Donner du texte à la div avec le message d'erreur
+    erreurDiv.textContent = message;
+  
+    // Insérer la div après le champ demandé
+    champ.parentNode.insertBefore(erreurDiv, champ.nextSibling);
+  }
+  
 
 
 
 // Attente du chargement de la page
 document.addEventListener('DOMContentLoaded', function(){
+// Sélectionner le formulaire à vérifier
+const formulaire = document.getElementById("register-form");
 
-    // Sélection du formulaire que nous souhaitons vérifier, nettoyage des erreurs
+// Créer 2 variables pour les champs de formulaire à vérifier
+const prenomChamp = document.getElementById("firstname");
+const nomChamp = document.getElementById("lastname");
 
+// Mettre un écouteur d'événement sur l'envoi du formulaire
+formulaire.addEventListener("submit", (event) => {
+  // Empêcher le comportement par défaut du formulaire
+  event.preventDefault();
 
+  // Nettoyer les messages d'erreurs existants
+  document.querySelectorAll(".field-invalid").forEach((element) => element.classList.remove("field-invalid"));
+  document.querySelectorAll(".error-text").forEach((element) => element.parentNode.removeChild(element));
 
-    // Vérification des champs
+  // Vérifier la longueur du prénom
+  if (prenomChamp.value.length < 2 || prenomChamp.value.length > 25) {
+    afficherErreur(prenomChamp, "Le prénom doit contenir entre 2 et 25 caractères !");
+  }
 
-
+  // Vérifier la longueur du nom
+  if (nomChamp.value.length < 2 || nomChamp.value.length > 25) {
+    afficherErreur(nomChamp, "Le nom doit contenir entre 2 et 25 caractères !");
+  }
+});
 });
--- JS/TP/TP Bonus/02-Thème sombre/Énoncé/js/script.js
+++ JS/TP/TP Bonus/02-Thème sombre/Énoncé/js/script.js
@@ -2,9 +2,39 @@
 
 // fonction qui permet de mettre en place ou supprimer la classe qui changera le thème de couleur de notre page
 
+// Fonction pour changer le thème
+function changerTheme(theme) {
+    // Récupérer l'élément body
+    const body = document.body;
+  
+    // Vérifier le thème demandé
+    if (theme === "dark") {
+      // Ajouter la classe dark au body
+      body.classList.add("dark-theme");
+      // Supprimer la classe light du body (si elle existe)
+      body.classList.remove("light-theme");
+    } else if (theme === "light") {
+      // Ajouter la classe light au body
+      body.classList.add("light-theme");
+      // Supprimer la classe dark du body (si elle existe)
+      body.classList.remove("dark-theme");
+    }
+  }
+  
+  // Écouteur d'événement pour le bouton Thème Clair
+  document.getElementById("theme-light").addEventListener("click", () => changerTheme("light"));
+  
+  // Écouteur d'événement pour le bouton Thème Sombre
+  document.getElementById("theme-dark").addEventListener("click", () => changerTheme("dark"));
+  
 
 // Écouteur d'évènement pour attendre le chargement complet du DOM
-document.addEventListener('DOMContentLoaded', function(){
-    // écoute des deux boutons au clic
+document.addEventListener('DOMContentLoaded', function(){// Écouteur d'événement pour le bouton Dark
+    document.getElementById("bouton-dark").addEventListener("click", () => changerTheme("dark"));
 
+    // Écouteur d'événement pour le bouton Light
+    document.getElementById("bouton-light").addEventListener("click", () => changerTheme("light"));
 });
+
+
+
