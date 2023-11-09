a04673db310a890deb4a0b3745e65a40a0ef6e34
Fournier Corinna <122286937+Corinna28@users.noreply.github.com>
2023-11-09 16:22:36 +0100

    maj

---
+++ JS/TP/TP Bonus/01-Formulaire/Consigne/Énoncé/Base/js/script.js
@@ -0,0 +1,18 @@
+
+
+// fonction pour mettre le champ demandé (field) en rouge (classe CSS "field-invalid") et insérer un message d'erreur (errorText) en dessous du champ qui aura la class css "error-text".
+
+
+
+
+// Attente du chargement de la page
+document.addEventListener('DOMContentLoaded', function(){
+
+    // Sélection du formulaire que nous souhaitons vérifier, nettoyage des erreurs
+
+
+
+    // Vérification des champs
+
+
+});
---
+++ JS/TP/TP Bonus/02-Thème sombre/consigne/js/script.js
@@ -0,0 +1,10 @@
+
+
+// fonction qui permet de mettre en place ou supprimer la classe qui changera le thème de couleur de notre page
+
+
+// Écouteur d'évènement pour attendre le chargement complet du DOM
+document.addEventListener('DOMContentLoaded', function(){
+    // écoute des deux boutons au clic
+
+});
